d3894228cf5bba7809759a6278916215a99a1e61
Robert Webb <bob.wbb@gmail.com>
2016-01-28 16:37:24 +0000

    added a 'Photo' model

--- photoviewer/models.py
+++ photoviewer/models.py
@@ -1,3 +1,17 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+#from django_extensions.db.models import TimeStampedModel
+
+
+class Photo(models.Model):
+    app_label = 'photoviewer'
+
+    caption = models.CharField(max_length=200)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        blank=False,
+    )
+
+    image = models.ImageField(upload_to='images')
